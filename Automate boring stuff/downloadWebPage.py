f1563513f09656691a285dd01b89bd24c341ea24
Tome Toyou <64913244+Tometoyou1983@users.noreply.github.com>
2020-08-20 16:24:15 -0400

    commit

---
+++ Automate boring stuff/downloadWebPage.py
@@ -0,0 +1,15 @@
+import os, requests
+
+os.system('cls')
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+#res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
+try:
+    res.raise_for_status()
+    playFile =  open('RomeoAndJuliet.txt', 'wb')
+    for chunk in res.iter_content(10000):
+        playFile.write(chunk)
+    playFile.close()
+except Exception as exc:
+    print('There was a problem: %s' %(exc))
+
